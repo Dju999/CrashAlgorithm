f8f6c73fba411629991415a92d627ecd1272b172
Dju999 <dju0204@gmail.com>
2017-10-26 16:13:30 +0300

    инициализация репозитория

---
+++ cracking_the_coding_interview/hackerrank.py
@@ -0,0 +1,14 @@
+def array_left_rotation(n, d, a):
+    """Shifts each of the array's elements  unit to the left
+    
+    https://www.hackerrank.com/challenges/ctci-array-left-rotation
+    
+    :param n: the number of integers
+    :param d: the number of left rortation
+    :param a: array
+    :return: a - rotated array
+    """
+
+if __name__=='__main__':
+    a = [1,2,3,4,5]
+    print(array_left_rotation(5,2,a))
